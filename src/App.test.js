52ba3a42d0740c25dea6775ffeaf989e254c3e31
JeffWinderAddons <jwinder@pond5.com>
2020-02-15 10:31:45 +0100

    initial commit

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,15 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import {
+  render
+} from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('expect app to be truthy', () => {
+
+  expect(App).toBeTruthy();
+});
+
+test('expect app to be truthy', () => {
+
+  expect(App).toBeTruthy();
 });
